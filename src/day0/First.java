7ab412611ca5144c43af352ce8ccde0c220c8123
Temuulen0528 <Temuulen0528@gmail.com>
2022-03-26 16:06:51 -0400

    lyalyalyaa

--- src/day0/First.java
+++ src/day0/First.java
@@ -4,6 +4,13 @@ public class First {
     public static void main(String[] args) {
         //todo: going to work on this tomorrow
         // add some cool code
-        //
+
+        //extra line 1
+        //extra line 2
+        //extra line 3
+
+
+
+
     }
 }
