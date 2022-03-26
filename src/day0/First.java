b4b0c46e3e5c18b8618542516f19d1ab484ce155
Temuulen0528 <Temuulen0528@gmail.com>
2022-03-26 16:05:35 -0400

    lyalyalyaa

---
+++ src/day0/First.java
@@ -0,0 +1,9 @@
+package day0;
+
+public class First {
+    public static void main(String[] args) {
+        //todo: going to work on this tomorrow
+        // add some cool code
+        //
+    }
+}
