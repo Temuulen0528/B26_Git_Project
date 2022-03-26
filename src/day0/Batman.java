5da2bd5bb92cab97ac4be764fc217156f920b4c1
Temuulen0528 <98855521+Temuulen0528@users.noreply.github.com>
2022-03-26 16:28:36 -0400

    Create Batman.java

---
+++ src/day0/Batman.java
@@ -0,0 +1,9 @@
+package day0;
+
+public class Batman{
+  public static void main(String [] args){
+    System.out.println("Batman stole my java knowledge");
+    System.out.println("Shame on you batboy");
+  }
+}
+  
